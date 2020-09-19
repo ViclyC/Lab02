d62b5345dcdeb702b747f71bcbe0e850bb4fca9e
A00090550 <arron_ferguson@bcit.ca>
2020-09-19 11:26:20 -0700

    Lab 02 for COMP 2522 Sept/2020, update 1.

--- src/bcit/computing/cst/comp2522/lab02/test/AnimalTest.java
+++ src/bcit/computing/cst/comp2522/lab02/test/AnimalTest.java
@@ -4,38 +4,10 @@ import bcit.computing.cst.comp2522.lab02.Cat;
 import bcit.computing.cst.comp2522.lab02.Dog;
 import bcit.computing.cst.comp2522.lab02.IncessantSpeakable;
 import bcit.computing.cst.comp2522.lab02.Poodle;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 
 public class AnimalTest {
 
-    @Test
-    public void testIncessantSpeakableOfCat() {
-        Cat c = new Cat();
-
-        assertEquals(false, (c instanceof IncessantSpeakable));
-    }
-
-    @Test
-    public void testIncessantSpeakableOfPoodle() {
-        Poodle p = new Poodle();
-
-        assertEquals(true, (p instanceof IncessantSpeakable));
-    }
-
-    @Test
-    public void testIncessantSpeakableOfDog() {
-        Dog d = new Dog();
-
-        assertEquals(false, (d instanceof IncessantSpeakable));
-    }
-
-    @Test
-    public void testDogSaysBarkBark() {
-        Dog d = new Dog();
-        assertEquals("Woof woof!", d.getSpeak());
-    }
 }
 
 
